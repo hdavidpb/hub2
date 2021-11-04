f3e3e9cb0c8c8cbcdd1693b544f65ce3453e98ef
hdavidpb <hernan.plazabs@gmail.com>
2021-11-04 16:33:54 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import imagen from "./assets/images/imagen.png";
 function App() {
+  localStorage.setItem("pruebaStorage", JSON.stringify("ABCDEFGHIJK123456"));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hub 2</h1>
+      <div className="cards__container">
+        <div className="card">
+          <img src={imagen} alt="proyect1" />
+          <a href="">Proyect 1</a>
+        </div>
+        <div className="card">
+          <img src={imagen} alt="proyect2" />
+          <a href="">Proyect 2</a>
+        </div>
+      </div>
     </div>
   );
 }
